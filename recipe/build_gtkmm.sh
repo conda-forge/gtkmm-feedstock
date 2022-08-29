0f846dfca079a979a078c7d2e1d5fe82ab5c7589
Tom Schoonjans <Tom.Schoonjans@genomicsplc.com>
2022-08-29 15:03:17 +0530

    Bump version to 3.24.6

--- recipe/build_gtkmm.sh
+++ recipe/build_gtkmm.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -ex
 
-./configure --prefix=$PREFIX
-make -j${CPU_COUNT}
-make install
+# get meson to find pkg-config when cross compiling
+export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+
+meson ${MESON_ARGS} \
+    --wrap-mode=nofallback \
+    --buildtype=release \
+    --prefix="${PREFIX}" \
+    -Dlibdir=lib \
+    builddir .
+
+ninja -C builddir -j${CPU_COUNT}
+ninja -C builddir test
+ninja -C builddir install
